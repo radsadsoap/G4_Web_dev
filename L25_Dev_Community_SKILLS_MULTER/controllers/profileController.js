8e3aa7dc9d438d430a8236ea9bd348a002bf8c62
Vinayak Sharma <vinayaksh8432@gmail.com>
2025-08-12 15:57:42 +0530

    12-08-2025

---
+++ L25_Dev_Community_SKILLS_MULTER/controllers/profileController.js
@@ -0,0 +1,47 @@
+const Profile = require("../models/profileModel");
+
+
+const getProfile = async (req,res)=>{
+    const data = await Profile.find({});
+    return res.status(200).json({ 
+        message: "All Profiles",
+        data
+    });
+
+
+}
+
+const createProfile = async (req,res)=> {  
+    const { imageUrl, summary, skills, workExperience, linkedinUrl, githubUrl, codingPlatform, resumeUrl} = req.body;
+
+    if (!imageUrl || !summary,! skills || !workExperience, !linkedinUrl || !githubUrl, !codingPlatform || !resumeUrl){
+        return res.status(400).send({ message: "Please Add all mandatory fields" });
+    }
+    // console.log(req.user);
+    
+
+    const newProfile = await Profile.create({
+        userId: req.user[0]._id,
+        imageUrl,
+        summary,
+        skills, 
+        workExperience,
+        linkedinUrl,
+        githubUrl, 
+        codingPlatform,
+        resumeUrl
+    })
+
+    const data = await newProfile.save();
+
+    return res.status(201).json({ 
+        message: "Profile Created",
+        data
+    });
+
+
+}
+
+
+
+module.exports = {getProfile, createProfile};
